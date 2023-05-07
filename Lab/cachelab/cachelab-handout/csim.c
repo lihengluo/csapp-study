28980329f83025def1c5897e1f980af9b783cfb4
shimmer147 <wailiheng@gmail.com>
2023-05-07 17:16:53 +0800

    update the Cache lab

---
+++ Lab/cachelab/cachelab-handout/csim.c
@@ -0,0 +1,15 @@
+#include "cachelab.h"
+#include "getopt.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+
+// s = 5, E = 1, b = 5
+// 2^s sets, 2^b bytes per block, E lines per set
+
+int main()
+{
+
+    printSummary(0, 0, 0);
+    return 0;
+}
